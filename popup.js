0281dad36c3790271c67461998167bf2be4a424a
SargeKhan <10beseukhan@seecs.edu.pk>
2015-08-30 20:57:37 +0500

    Adding Styles

--- popup.js
+++ popup.js
@@ -62,23 +62,20 @@ function editList()
                 console.log("List iS:" + list);
                 bodyData= "<h3 id="+ list+"_h>" + list +"</h3>";
                 var it=0;
+		if(vb.urls[list].length==0)
+	   		bodyData= bodyData+ "<div> This list is empty</div>";
                 vb.urls[list].forEach(function(obj){
                         console.log(obj.length);
-                        var c = obj.substr(0, 30);
+                        var c = obj;
                         var dots= "...";
                         c= c+ dots;
                         console.log(c);
-                        bodyData=  bodyData+"<div id=\""+it+ "\">"+c+ "<button class=\"pure-button\" id=\"deleteB_"+it+"\"> Delete </button> </br></div>";
+                        bodyData=  bodyData+"<div id=\""+it+ "\">"+c+ "<button class=\"btn btn-danger\" id=\"deleteB_"+it+"\"> Delete </button> </br></div>";
                         console.log("Iteration: \n"+bodyData);
-                    /*else{
-                        var c = obj;
-                        bodyData= bodyData + c + "<button class=\"pure-button\" id=\"b_"+it+"> Delete </button>";
-                        console.log(c);
-                    }*/
                     it++;
                 });
 		it=0;
-                bodyData= bodyData+ "<br> <button id='go' class=\"pure-button\"> Go Back </button>";
+                bodyData= bodyData+ "<br> <button id='go' class=\"btn btn-block\"> Collapse Group </button>";
 		bodyData= "<div id='details'>" + bodyData;
 		bodyData= bodyData + "</div>";
                 console.log("Outside Loop" + bodyData);
@@ -126,11 +123,11 @@ function createTable(){
     chrome.storage.local.get("urls", function(vb){
         if(JSON.stringify(vb)=="{}")
         {
-            chrome.storage.local.set({'urls':{"deft": []}});
+            chrome.storage.local.set({'urls':{"default": []}});
             vb={'urls':{"defaults": []}};
         }
         for(var x in vb.urls) {
-            tableData=tableData+  "<tr><td align=\"center\"><button class=\"pure-button\" align=\"center\" id=\""+x+"\">"+x+"</td><td> <button class=\"pure-button\" id=\""+x+"_s\"> Add Current Page</button></td><td><button class=\"pure-button\" id=\""+x+"_e\">Edit List</button></td></tr>";
+            tableData=tableData+  "<tr><td align=\"center\"><button class=\"btn btn-default\" style=\"width:100%\" id=\""+x+"\">"+x+"</a></td><td> <button class=\"btn btn-success\" id=\""+x+"_s\"> Add Current Page</button></td><td><button class=\"btn btn-info\" id=\""+x+"_e\">Edit List</button></td></tr>";
         }
         tab.innerHTML= tableData;
         
